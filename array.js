0c12cedc2a759ade7ccc4152ad431ac7d395cc50
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-02-27 12:54:52 -0500

    resize, push started

--- array.js
+++ array.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const memory = require('./memory');
 const Memory = new memory();
 
@@ -7,16 +9,31 @@ class MyArray {
     this.ptr = Memory.allocate(this.length);
     this.capacity = 0;
   }
+  push(value) {
+    Memory.set(this.ptr + this.length, value);
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = Memory.allocate(size * MyArray.SIZE_RATIO);
+    Memory.copy(this.ptr, oldPtr, )
+    Memory.free(oldPtr);
+  }
 
   get(index) {
     if (index < 0 || index >= this.length) {
       throw new Error('not enough length or its negative 🤷🏻‍');
     }
-
     return Memory.get(this.ptr + index);
   }
 }
 
+MyArray.SIZE_RATIO = 3;
+
 const array = new MyArray();
 
+array.push(5);
+const five = array.get(0);
+
 console.log(array);
+
--- memory.js
+++ memory.js
@@ -1,3 +1,6 @@
+'use strict';
+
+
 class Memory {
   constructor() {
     this.memory = new Float64Array(1024);
