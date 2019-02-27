428dafcd82b5aa6360d149490beca7f5c0ba3ad1
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-02-27 15:38:45 -0500

    remove less than 5

--- realArrayDrills.js
+++ realArrayDrills.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const replaceSpace = str => {
   //loops over str
   //checks each val to makes sure if there is an empty space
@@ -7,3 +9,26 @@ const replaceSpace = str => {
 };
 
 console.log(replaceSpace('www.thinkful.com /tauh ida parv een'));
+
+
+// Filter an array
+
+
+//  array of numbers,
+//  remove all numbers that are less than 5
+
+
+const removeFourAndBelow = (arr) => {
+
+  // loop over array
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 5) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
+};
+
+const arr = [6,6,6,3,7,7,7,2];
+
+console.log(removeFourAndBelow(arr));
