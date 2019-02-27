04b5b74831ee27ec4303333b2f356818f96756e8
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-02-27 16:33:13 -0500

    merge arrays

--- realArrayDrills.js
+++ realArrayDrills.js
@@ -42,4 +42,25 @@ const maxSum = arr => {
   return max;
 };
 
-console.log(maxSum([-4, -6, -3, -5, -2, 1]));
+// console.log(maxSum([-4, -6, -3, -5, -2, 1]));
+
+
+//  merge arrays
+
+const mergeArrays = (arr1, arr2) => {
+  // array .concat
+
+  const newArray = arr1.concat(arr2);
+  const otherNewArray = [...arr1, ...arr2];
+
+  return otherNewArray.sort((a, b) => a - b);
+
+  //array sort a-b
+  //return
+};
+
+const arr1 = [1, 3, 6, 8, 11];
+
+const arr2 = [2, 3, 5, 8, 9, 10];
+
+// console.log(mergeArrays(arr1, arr2));
---
+++ scratch.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+
+const findMaxSum = (arr) => {
+  let maxSum;
+  let counter = 0;
+
+  if(counter === arr.length){
+    return maxSum;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if i
+  }
+};
